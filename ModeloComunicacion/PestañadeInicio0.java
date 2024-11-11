65d5e5c069d6e24ab9d5162c5492e73a7c7a0d6f
R4gn4r0kz <diegotomas124@gmail.com>
2024-11-11 18:13:21 -0300

    Update PestañadeInicio0.java

--- ModeloComunicacion/PestañadeInicio0.java
+++ ModeloComunicacion/PestañadeInicio0.java
@@ -9,5 +9,82 @@ package ModeloComunicacion;
  * @author diego
  */
 public class PestañadeInicio0 {
-    
+   public class PestañadeInicio0 {
+    private String Ingresaralsistema;
+    private String BuscarProductos;
+    private String GestionarCarrito;
+    private int pasareladepago;
+    private int PagarProducto;
+    private String IngresarDirreccion;
+    private String Seguimiento;
+
+    public PestañadeInicio0() {
+    }
+
+    public PestañadeInicio0(String Ingresaralsistema, String BuscarProductos, String GestionarCarrito, int pasareladepago, int PagarProducto, String IngresarDirreccion, String Seguimiento) {
+        this.Ingresaralsistema = Ingresaralsistema;
+        this.BuscarProductos = BuscarProductos;
+        this.GestionarCarrito = GestionarCarrito;
+        this.pasareladepago = pasareladepago;
+        this.PagarProducto = PagarProducto;
+        this.IngresarDirreccion = IngresarDirreccion;
+        this.Seguimiento = Seguimiento;
+    }
+
+    public String getIngresaralsistema() {
+        return Ingresaralsistema;
+    }
+
+    public void setIngresaralsistema(String Ingresaralsistema) {
+        this.Ingresaralsistema = Ingresaralsistema;
+    }
+
+    public String getBuscarProductos() {
+        return BuscarProductos;
+    }
+
+    public void setBuscarProductos(String BuscarProductos) {
+        this.BuscarProductos = BuscarProductos;
+    }
+
+    public String getGestionarCarrito() {
+        return GestionarCarrito;
+    }
+
+    public void setGestionarCarrito(String GestionarCarrito) {
+        this.GestionarCarrito = GestionarCarrito;
+    }
+
+    public int getPasareladepago() {
+        return pasareladepago;
+    }
+
+    public void setPasareladepago(int pasareladepago) {
+        this.pasareladepago = pasareladepago;
+    }
+
+    public int getPagarProducto() {
+        return PagarProducto;
+    }
+
+    public void setPagarProducto(int PagarProducto) {
+        this.PagarProducto = PagarProducto;
+    }
+
+    public String getIngresarDirreccion() {
+        return IngresarDirreccion;
+    }
+
+    public void setIngresarDirreccion(String IngresarDirreccion) {
+        this.IngresarDirreccion = IngresarDirreccion;
+    }
+
+    public String getSeguimiento() {
+        return Seguimiento;
+    }
+
+    public void setSeguimiento(String Seguimiento) {
+        this.Seguimiento = Seguimiento;
+    }
+     
 }
