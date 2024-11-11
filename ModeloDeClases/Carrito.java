86e02f55201f5c7d437b692eb20e775b14380189
R4gn4r0kz <diegotomas124@gmail.com>
2024-11-11 18:19:25 -0300

    Add files via upload

---
+++ ModeloDeClases/Carrito.java
@@ -0,0 +1,73 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ModeloClases;
+
+/**
+ *
+ * @author diego
+ */
+public class Carrito {
+    private int IDcarrito;
+    private String ListadeProductos;
+    private int Total;
+    private int ActualizarStock;
+    private int ObtenerPrecio;
+
+    public Carrito() {
+    }
+    
+    
+
+    public Carrito(int IDcarrito, String ListadeProductos, int Total, int ActualizarStock, int ObtenerPrecio) {
+        this.IDcarrito = IDcarrito;
+        this.ListadeProductos = ListadeProductos;
+        this.Total = Total;
+        this.ActualizarStock = ActualizarStock;
+        this.ObtenerPrecio = ObtenerPrecio;
+    }
+
+    public int getIDcarrito() {
+        return IDcarrito;
+    }
+
+    public void setIDcarrito(int IDcarrito) {
+        this.IDcarrito = IDcarrito;
+    }
+
+    public String getListadeProductos() {
+        return ListadeProductos;
+    }
+
+    public void setListadeProductos(String ListadeProductos) {
+        this.ListadeProductos = ListadeProductos;
+    }
+
+    public int getTotal() {
+        return Total;
+    }
+
+    public void setTotal(int Total) {
+        this.Total = Total;
+    }
+
+    public int getActualizarStock() {
+        return ActualizarStock;
+    }
+
+    public void setActualizarStock(int ActualizarStock) {
+        this.ActualizarStock = ActualizarStock;
+    }
+
+    public int getObtenerPrecio() {
+        return ObtenerPrecio;
+    }
+
+    public void setObtenerPrecio(int ObtenerPrecio) {
+        this.ObtenerPrecio = ObtenerPrecio;
+    }
+    
+    
+      
+}
